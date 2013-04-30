7c0f686e59b5b389df6e3706b0c7a80fb35668e3
David Vannucci <david.vannucci@seecrypt.com>
2013-04-30 22:25:37 +0200

    excluding gen directory

--- gen/za/co/appceptional/android/ingressintel/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package za.co.appceptional.android.ingressintel;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
